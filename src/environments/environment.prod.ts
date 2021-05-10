92ada814b9b3e1c41f5cffe4a134649aa29823d2
Nirmalya247 <nrmlgn247@gmail.com>
2021-05-11 01:10:10 +0530

    a1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  PATH: "https://thefarmerguy.herokuapp.com/",
+  PATH: "https://medventory-backend.herokuapp.com/",
   SESSION_ID: 'SESSION_ID',
   SESSION_USERID: 'SESSION_USERID'
 };
