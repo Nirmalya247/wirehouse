acd6fdfdd82ec41810a954d1fa23d979a7d48e61
Nirmalya247 <nrmlgn247@gmail.com>
2021-05-23 14:29:00 +0530

    a1

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -21,10 +21,12 @@ import { Item } from '../../data/item';
 })
 export class DashboardComponent implements OnInit {
     totalItemSold: string = '0';
+    totalItemBought: string = '0';
     totalEarning: string = '0.00';
     totalSpending: string = '0.00';
+    timString: string = 'Daily';
 
-    graphBy: string = 'month';
+    graphBy: string = 'day';
 
     constructor(
         public authGuardService: AuthGuardService,
@@ -42,14 +44,322 @@ export class DashboardComponent implements OnInit {
         this.getCreditCount();
         // generate random values for mainChart
     }
-    
+
+
+    // today temp
+    // itemSoldChart
+    public itemSoldChartData: Array<any> = [
+        {
+            data: [1, 18, 9, 17, 34, 22, 11],
+            label: 'Series A'
+        }
+    ];
+    public itemSoldChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public itemSoldChartOptions: any = {
+        tooltips: {
+            enabled: false,
+            custom: CustomTooltips
+        },
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: [{
+                display: false
+            }],
+            yAxes: [{
+                display: false
+            }]
+        },
+        elements: {
+            line: {
+                borderWidth: 2
+            },
+            point: {
+                radius: 0,
+                hitRadius: 10,
+                hoverRadius: 4,
+            },
+        },
+        legend: {
+            display: false
+        }
+    };
+    public itemSoldChartColours: Array<any> = [
+        { // grey
+            backgroundColor: getStyle('--info'),
+            borderColor: 'rgba(255,255,255,.55)'
+        }
+    ];
+    public itemSoldChartLegend = false;
+    public itemSoldChartType = 'line';
+
+
+    // earningChart
+    public earningChartData: Array<any> = [
+        {
+            data: [65, 59, 84, 84, 51, 55, 40],
+            label: 'Series A'
+        }
+    ];
+    public earningChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public earningChartOptions: any = {
+        tooltips: {
+            enabled: false,
+            custom: CustomTooltips
+        },
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: [{
+                display: false
+            }],
+            yAxes: [{
+                display: false
+            }]
+        },
+        elements: {
+            line: {
+                borderWidth: 2
+            },
+            point: {
+                radius: 0,
+                hitRadius: 10,
+                hoverRadius: 4,
+            },
+        },
+        legend: {
+            display: false
+        }
+    };
+    public earningChartColours: Array<any> = [
+        {
+            backgroundColor: getStyle('--primary'),
+            borderColor: 'rgba(255,255,255,.55)'
+        }
+    ];
+    public earningChartLegend = false;
+    public earningChartType = 'line';
+
+
+    // itemBoughtChart
+    public itemBoughtChartData: Array<any> = [
+        {
+            data: [78, 81, 80, 45, 34, 12, 40],
+            label: 'Series A'
+        }
+    ];
+    public itemBoughtChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public itemBoughtChartOptions: any = {
+        tooltips: {
+            enabled: false,
+            custom: CustomTooltips
+        },
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: [{
+                display: false
+            }],
+            yAxes: [{
+                display: false
+            }]
+        },
+        elements: {
+            line: {
+                borderWidth: 2
+            },
+            point: {
+                radius: 0,
+                hitRadius: 10,
+                hoverRadius: 4,
+            },
+        },
+        legend: {
+            display: false
+        }
+    };
+    public itemBoughtChartColours: Array<any> = [
+        {
+            backgroundColor: 'rgba(255,255,255,.2)',
+            borderColor: 'rgba(255,255,255,.55)',
+        }
+    ];
+    public itemBoughtChartLegend = false;
+    public itemBoughtChartType = 'line';
+
+
+    // spendingChart
+    public spendingChartData: Array<any> = [
+        {
+            data: [78, 81, 80, 45, 34, 12, 40, 78, 81, 80, 45, 34, 12, 40, 12, 40],
+            label: 'Series A',
+            barPercentage: 0.6,
+        }
+    ];
+    public spendingChartLabels: Array<any> = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'];
+    public spendingChartOptions: any = {
+        tooltips: {
+            enabled: false,
+            custom: CustomTooltips
+        },
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: [{
+                display: false
+            }],
+            yAxes: [{
+                display: false
+            }]
+        },
+        elements: {
+            line: {
+                borderWidth: 2
+            },
+            point: {
+                radius: 0,
+                hitRadius: 10,
+                hoverRadius: 4,
+            },
+        },
+        legend: {
+            display: false
+        }
+    };
+    public spendingChartColours: Array<any> = [
+        {
+            backgroundColor: 'rgba(255,255,255,.3)',
+            borderWidth: 0
+        }
+    ];
+    public spendingChartLegend = false;
+    public spendingChartType = 'line';
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // graph
+    // mainChart
+
+    public mainChartElements = 27;
+    public mainChartData1: Array<number> = [];
+    public mainChartData2: Array<number> = [];
+    public mainChartData3: Array<number> = [];
+
+    public mainChartData: Array<any> = [
+        {
+            data: this.mainChartData1,
+            label: 'Current'
+        },
+        {
+            data: this.mainChartData2,
+            label: 'Previous'
+        },
+        {
+            data: this.mainChartData3,
+            label: 'BEP'
+        }
+    ];
+    /* tslint:disable:max-line-length */
+    public mainChartLabels: Array<any> = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Thursday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    /* tslint:enable:max-line-length */
+    public mainChartOptions: any = {
+        tooltips: {
+            enabled: false,
+            custom: CustomTooltips,
+            intersect: true,
+            mode: 'index',
+            position: 'nearest',
+            callbacks: {
+                labelColor: function (tooltipItem, chart) {
+                    return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].borderColor };
+                }
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    drawOnChartArea: false,
+                }/*,
+          ticks: {
+            callback: function(value: any) {
+              return value.charAt(0);
+            }
+          }*/
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    maxTicksLimit: 5,
+                    stepSize: Math.ceil(250 / 5)
+                }
+            }]
+        },
+        elements: {
+            line: {
+                borderWidth: 2
+            },
+            point: {
+                radius: 0,
+                hitRadius: 10,
+                hoverRadius: 4,
+                hoverBorderWidth: 3,
+            }
+        },
+        legend: {
+            display: false
+        }
+    };
+    public mainChartColours: Array<any> = [
+        { // brandSuccess
+            backgroundColor: 'transparent',
+            borderColor: getStyle('--success'),
+            pointHoverBackgroundColor: '#fff',
+            borderWidth: 1,
+            borderDash: [8, 5]
+        },
+        { // brandDanger
+            backgroundColor: 'transparent',
+            borderColor: getStyle('--danger'),
+            pointHoverBackgroundColor: '#fff',
+            borderWidth: 1,
+            borderDash: [8, 5]
+        },
+        { // brandInfo
+            backgroundColor: hexToRgba(getStyle('--info'), 10),
+            borderColor: getStyle('--info'),
+            pointHoverBackgroundColor: '#fff'
+        }
+    ];
+    public mainChartLegend = false;
+    public mainChartType = 'line';
+
+
+
+
+
+
+
+
+
+
+
+
 
     // today
     getTodayData() {
-        this.dashboardDataService.getToday({ }).subscribe(res => {
+        this.dashboardDataService.getToday({}).subscribe(res => {
             // console.log(res);
             if (!res.err) {
                 this.totalItemSold = res.itemsold;
+                this.totalItemBought = res.itembought;
                 this.totalEarning = res.earning;
                 this.totalSpending = res.spending;
             }
@@ -57,61 +367,39 @@ export class DashboardComponent implements OnInit {
     }
 
 
+
+
     // graph
     getGraphData() {
         this.dashboardDataService.getGraphData({ findby: this.graphBy }).subscribe(res => {
             // console.log(res);
             if (!res.err) {
-                this.barChartLabels = res.labels;
-                this.barChartData = [
+                this.mainChartLabels = res.labels;
+                this.mainChartData = [
                     { data: res.earning, label: 'earning' },
                     { data: res.spending, label: 'spending' },
                     { data: res.profit, label: 'profit' }
                 ];
+                this.itemSoldChartData = [{ data: res.itemsold, label: 'itemsold' }];
+                this.itemSoldChartLabels = res.labels;
+                this.spendingChartData = [{ data: res.spending, label: 'spending' }];
+                this.spendingChartLabels = res.labels;
+                this.itemBoughtChartData = [{ data: res.itembought, label: 'itembought' }];
+                this.itemBoughtChartLabels = res.labels;
+                this.earningChartData = [{ data: res.earning, label: 'earning' }];
+                this.earningChartLabels = res.labels;
+                this.totalItemSold = res.now.itemsold;
+                this.totalItemBought = res.now.itembought;
+                this.totalEarning = res.now.earning;
+                this.totalSpending = res.now.spending;
+
+                if (this.graphBy == 'day') this.timString = 'Daily';
+                else if (this.graphBy == 'month') this.timString = 'Monthly';
+                else if (this.graphBy == 'year') this.timString  = 'Yearly'
             }
         })
     }
 
-    
-    public barChartOptions: any = {
-        scaleShowVerticalLines: false,
-        responsive: true
-    };
-    public barChartLabels: string[] = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JULY', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-    public barChartType = 'bar';
-    public barChartLegend = true;
-    public barChartColours: Array<any> = [
-        { // blue
-            backgroundColor: 'rgb(75, 0, 130, 0.5)',
-            borderColor: 'rgb(75, 0, 130, 1)',
-            pointBackgroundColor: 'rgb(75, 0, 130, 1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(75, 0, 130, 1)'
-        },
-        { // red
-            backgroundColor: 'rgb(255, 146, 0, 0.5)',
-            borderColor: 'rgb(255, 146, 0, 1)',
-            pointBackgroundColor: 'rgb(255, 146, 0, 1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(255, 146, 0, 1)'
-        },
-        { // green
-            backgroundColor: 'rgba(36, 189, 77, 0.5)',
-            borderColor: 'rgba(36, 189, 77,1)',
-            pointBackgroundColor: 'rgba(36, 189, 77,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(36, 189, 77, 1)'
-        }
-    ];
-
-    public barChartData: any[] = [
-        { data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], label: 'earning' },
-        { data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], label: 'spending' },
-        { data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], label: 'profit' }
-    ];
     public chartClicked(e: any): void {
         // console.log(e);
     }
@@ -123,12 +411,12 @@ export class DashboardComponent implements OnInit {
     dataLimit = 5;
     // expiry
     expiryOrder = 'asc';
-    expiryData = [ ];
-    expiryPages = [ 1 ];
+    expiryData = [];
+    expiryPages = [1];
     expiryPage = 1;
     getExpiryCount() {
-        this.dashboardDataService.getExpiryCount({ }).subscribe(res => {
-            this.expiryPages = Array.from({length: Math.ceil(parseInt(res.toString()) / this.dataLimit)}, (_, i) => i + 1);
+        this.dashboardDataService.getExpiryCount({}).subscribe(res => {
+            this.expiryPages = Array.from({ length: Math.ceil(parseInt(res.toString()) / this.dataLimit) }, (_, i) => i + 1);
             this.getExpiry(null);
             // console.log(res);
         })
@@ -152,12 +440,12 @@ export class DashboardComponent implements OnInit {
     }
     // stock
     stockOrder = 'asc';
-    stockData: Array<Item> = [ ];
-    stockPages = [ 1 ];
+    stockData: Array<Item> = [];
+    stockPages = [1];
     stockPage = 1;
     getStockCount() {
-        this.dashboardDataService.getStockCount({ }).subscribe(res => {
-            this.stockPages = Array.from({length: Math.ceil(parseInt(res.toString()) / this.dataLimit)}, (_, i) => i + 1);
+        this.dashboardDataService.getStockCount({}).subscribe(res => {
+            this.stockPages = Array.from({ length: Math.ceil(parseInt(res.toString()) / this.dataLimit) }, (_, i) => i + 1);
             this.getStock(null);
             // console.log(res);
         })
@@ -176,13 +464,13 @@ export class DashboardComponent implements OnInit {
     }
     // demand
     demandOrder = 'desc';
-    demandData = [ ];
-    demandPages = [ 1 ];
+    demandData = [];
+    demandPages = [1];
     demandPage = 1;
     getDemandCount() {
-        this.dashboardDataService.getDemandCount({ }).subscribe(res => {
-            this.demandPages = Array.from({length: Math.ceil(parseInt(res.toString()) / this.dataLimit)}, (_, i) => i + 1);
-            this.earningPages = Array.from({length: Math.ceil(parseInt(res.toString()) / this.dataLimit)}, (_, i) => i + 1);
+        this.dashboardDataService.getDemandCount({}).subscribe(res => {
+            this.demandPages = Array.from({ length: Math.ceil(parseInt(res.toString()) / this.dataLimit) }, (_, i) => i + 1);
+            this.earningPages = Array.from({ length: Math.ceil(parseInt(res.toString()) / this.dataLimit) }, (_, i) => i + 1);
             this.getDemand(null);
             this.getEarning(null);
             console.log(res);
@@ -202,8 +490,8 @@ export class DashboardComponent implements OnInit {
     }
     // earning
     earningOrder = 'desc';
-    earningData = [ ];
-    earningPages = [ 1 ];
+    earningData = [];
+    earningPages = [1];
     earningPage = 1;
     getEarning(pageNo) {
         if (pageNo != null) {
@@ -219,12 +507,12 @@ export class DashboardComponent implements OnInit {
     }
     // credit
     creditOrder = 'asc';
-    creditData = [ ];
-    creditPages = [ 1 ];
+    creditData = [];
+    creditPages = [1];
     creditPage = 1;
     getCreditCount() {
-        this.dashboardDataService.getCreditCount({ }).subscribe(res => {
-            this.creditPages = Array.from({length: Math.ceil(parseInt(res.toString()) / this.dataLimit)}, (_, i) => i + 1);
+        this.dashboardDataService.getCreditCount({}).subscribe(res => {
+            this.creditPages = Array.from({ length: Math.ceil(parseInt(res.toString()) / this.dataLimit) }, (_, i) => i + 1);
             this.getCredit(null);
             console.log(res);
         })
