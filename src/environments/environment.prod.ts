dd45fd91802c473b31b09f6c0923b5ae070c34c9
Nirmalya247 <nrmlgn247@gmail.com>
2021-07-21 12:15:00 +0530

    f1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  PATH: "https://med.kalyanimedical.com/",
+  // PATH: "https://med.kalyanimedical.com/",
+  PATH: "https://medventory-backend.herokuapp.com/",
   SESSION_ID: 'SESSION_ID',
   SESSION_USERID: 'SESSION_USERID'
 };
